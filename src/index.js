98232f71b4dd982f58bc1dd92544a5b8884f0f1d
Paola Rosano <paodiazglez.pdgr@gmail.com>
2022-07-22 18:27:39 -0500

    ejempo Carlos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import validator from './validator.js';
+
 document.getElementById("confirmar").addEventListener("click", () => { 
   validar(document.getElementById("cardNumber").value);
 });
+const titulo = document.getElementById("titulo")// acceso al dom
+const ejembtn=document.getElementById('eje') // acceso al dom
+ejembtn.addEventListener('click', function(){ // agregamos un evento alboton
+  titulo.innerHTML=validator.ejemplo("Laura Cano") // 
+})
 
 const validar = (numero) => {
   if (numero === "") {
-    return;
+    document.getElementById("error").innerHTML = "Completa este campo";
   } else {
     let pattern=/^\d{16}$/ // expresiones regulares
     if (pattern.test(numero)) {
---
+++ src/validator.js
@@ -0,0 +1,13 @@
+const validator = {
+    isValid: function (){
+        console.log("si se puede chicas")
+    },
+   maskify:function(){
+
+   },
+   ejemplo:function(nombre){
+    return "Hola "+nombre+" saludos jaja"
+   },
+   nombre:"malefica"
+  };
+  export default validator;
