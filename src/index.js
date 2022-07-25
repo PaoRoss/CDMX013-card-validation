6594640558d90bd1d0b61b6293089008a3b67c19
Paola Rosano <paodiazglez.pdgr@gmail.com>
2022-07-25 18:27:35 -0500

    comentarios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import validator from './validator.js';
 
+// Declarando variables
 const btnConfirmar = document.getElementById('confirmar');
 const numberAccount = document.getElementById("cardNumber");
 
-
+// Agregando eventos
 
 btnConfirmar.addEventListener('click', validarCuenta);
+
+// Creando función para manipular el DOM
 
 function validarCuenta() {
   if(numberAccount.value.length <= 12){
@@ -20,7 +23,9 @@ function validarCuenta() {
   limpiar();
 }
 
-function limpiar(){
+// Función para limpiar el mensaje
+
+function limpiar(){ 
   setTimeout( () => {
     document.getElementById('error').innerHTML = '';
   }, 2000);
